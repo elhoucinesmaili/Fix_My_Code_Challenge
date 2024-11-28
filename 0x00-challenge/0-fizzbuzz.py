209ff04de779b631ebacc50b2599ce5bd54185e6
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-11-28 09:15:29 -0800

    hdgdg

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,26 +1,43 @@
 #!/usr/bin/python3
+"""
+FizzBuzz
+    A simple FizzBuzz implementation.
+"""
+
 import sys
 
+
 def fizzbuzz(n):
+    """
+    Prints numbers from 1 to n, replacing:
+    - Multiples of 3 with "Fizz".
+    - Multiples of 5 with "Buzz".
+    - Multiples of both 3 and 5 with "FizzBuzz".
+    """
+    if n < 1:
+        return
+
     for i in range(1, n + 1):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end=" ")
-        elif i % 3 == 0:
-            print("Fizz", end=" ")
-        elif i % 5 == 0:
-            print("Buzz", end=" ")
-        else:
-            print(i, end=" ")
+        output = ""
+        if i % 3 == 0:
+            output += "Fizz"
+        if i % 5 == 0:
+            output += "Buzz"
+        print(output or i, end=" ")
+    print()  # Newline after the loop
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
+        print("Missing number")
         print("Usage: ./0-fizzbuzz.py <number>")
+        print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
 
     try:
-        num = int(sys.argv[1])
+        number = int(sys.argv[1])
     except ValueError:
-        print("Please provide an integer.")
+        print("Invalid input: Please enter an integer.")
         sys.exit(1)
 
-    fizzbuzz(num)
+    fizzbuzz(number)
