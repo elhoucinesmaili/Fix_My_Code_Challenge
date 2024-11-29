76bb7f36236f6c8b6e497b7ffefb999367f794f1
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-11-29 08:54:22 -0800

    dgre

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,35 +1,36 @@
 #!/usr/bin/python3
-"""
-FizzBuzz Challenge
+""" FizzBuzz
 """
 import sys
 
 
 def fizzbuzz(n):
     """
-    Prints numbers from 1 to n separated by a space, with substitutions:
-    - "Fizz" for multiples of 3,
-    - "Buzz" for multiples of 5,
-    - "FizzBuzz" for multiples of both 3 and 5.
+    FizzBuzz function prints numbers from 1 to n separated by a space.
+
+    - For multiples of three print "Fizz" instead of the number and for
+      multiples of five print "Buzz".
+    - For numbers which are multiples of both three and five print "FizzBuzz".
     """
     if n < 1:
         return
 
-    result = []
+    tmp_result = []
     for i in range(1, n + 1):
-        output = ""
-        if i % 3 == 0:
-            output += "Fizz"
-        if i % 5 == 0:
-            output += "Buzz"
-        result.append(output if output else str(i))
-    
-    print(" ".join(result))
+        if (i % 3 == 0 and i % 5 == 0):  # Check for multiples of both 3 and 5 first
+            tmp_result.append("FizzBuzz")
+        elif (i % 3 == 0):  # Check for multiples of 3
+            tmp_result.append("Fizz")
+        elif (i % 5 == 0):  # Check for multiples of 5
+            tmp_result.append("Buzz")
+        else:
+            tmp_result.append(str(i))
+    print(" ".join(tmp_result))
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Missing or invalid number")
+    if len(sys.argv) <= 1:
+        print("Missing number")
         print("Usage: ./0-fizzbuzz.py <number>")
         print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
@@ -38,5 +39,6 @@ if __name__ == '__main__':
         number = int(sys.argv[1])
         fizzbuzz(number)
     except ValueError:
-        print("The argument must be an integer.")
+        print("Please provide a valid number")
         sys.exit(1)
+
