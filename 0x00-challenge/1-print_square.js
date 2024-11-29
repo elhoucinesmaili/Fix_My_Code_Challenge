e12903d433fc5b7927373815b997cd7eafe6baa3
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-11-29 08:34:41 -0800

    dgdbd

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -5,21 +5,17 @@
     The size of the square must be the first argument 
     of the program.
 */
-
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1);
+    process.exit(1)
 }
 
-const size = parseInt(process.argv[2], 10);
-
-if (isNaN(size) || size <= 0) {
-    process.stderr.write("Size must be a positive integer\n");
-    process.exit(1);
-}
-
-for (let i = 0; i < size; i++) {
-    process.stdout.write("#".repeat(size) + "\n");
+let size = parseInt(process.argv[2], 10)
+for (let i = 0 ; i < size ; i ++) {
+    for (let j = 0 ; j < size ; j ++) {
+        process.stdout.write("#");
+    }
+    process.stdout.write("\n");
 }
