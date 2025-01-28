6a5e088eaf70c4002f3e46dc5a9c2ef9d7d343d8
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2025-01-28 09:21:24 -0800

    git

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,31 +2,29 @@
 """ Square module """
 
 
-class Square():
-    """ A square class """
-    width = 0
-    height = 0
+class Square:
+    """Square class"""
 
-    def __init__(self, *args, **kwargs):
-        """ the init for the square class """
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    def __init__(self, width=0, height=0):
+        """Initialize the square with width and height"""
+        self.width = width
+        self.height = height
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """Calculate area of the square"""
         return self.width * self.height
 
     def perimeter_of_my_square(self):
-        """ the perimeter of the square """
+        """Calculate perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ the str rep """
+        """String representation of the square"""
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    s = Square(width=12, height=9)
+    s = Square(width=12, height=8)  # Example values
     print(s)
     print(s.area_of_my_square())
     print(s.perimeter_of_my_square())
