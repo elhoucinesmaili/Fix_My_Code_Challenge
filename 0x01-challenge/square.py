cec438e6d1750d7e4cf8f2d98720454a21da2266
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2025-01-28 09:54:47 -0800

    done

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,33 +1,29 @@
-#!/usr/bin/python3
-""" Square module """
-
 class Square:
-    """ A square class """
+    """A square class"""
+    
     width = 0
     height = 0
 
-    def __init__(self, *args, **kwargs):
-        """ Initialize the square class """
+    def __init__(self, **kwargs):
+        """The init for the square class"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
-        # Ensure the square's area uses the `width` dimension only for a perfect square
-        return self.width * self.width
+        """Area of the square"""
+        return self.width * self.height
 
     def perimeter_of_my_square(self):
-        """ The perimeter of the square """
-        # Use `width` only for the perimeter of a perfect square
-        return self.width * 4
+        """The perimeter of the square"""
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ String representation """
+        """The string representation"""
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    s = Square(width=12, height=9)  # Instantiate the class
-    print(s)  # Print the string representation
-    print(s.area_of_my_square())  # Print the area
-    print(s.perimeter_of_my_square())  # Print the perimeter
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.perimeter_of_my_square())
